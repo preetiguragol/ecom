12741f0c86a23e23b57073afeb462ffefbee217e
preetiguragol <preetiguragol@gmail.com>
2023-08-11 22:53:19 +0530

    links updates

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,35 +6,35 @@ const Footer = () => {
    <p className='p-style'>
       <div className='footer-margin'>
       <ol>
-      <ul><a className='links-style' href='About'>Contact</a> </ul>
-      <ul><a className='links-style' href='About'>Help</a> </ul>
-      <ul><a className='links-style' href='About'>CutomerCare</a> </ul>
+      <ul><a className='links-style' href='/About'>Contact</a> </ul>
+      <ul><a className='links-style' href='/About'>Help</a> </ul>
+      <ul><a className='links-style' href='/About'>CutomerCare</a> </ul>
       </ol>
       </div>
       <div className='footer-margin'>
       <ol>
-      <ul><a className='links-style' href='About'>WhoAreWe</a> </ul>
-      <ul><a className='links-style' href='About'>Careers</a> </ul>
-      <ul><a className='links-style' href='About'>Authenticity</a> </ul>
-      <ul><a className='links-style' href='About'>Press</a> </ul>
+      <ul><a className='links-style' href='/About'>WhoAreWe</a> </ul>
+      <ul><a className='links-style' href='/About'>Careers</a> </ul>
+      <ul><a className='links-style' href='/About'>Authenticity</a> </ul>
+      <ul><a className='links-style' href='/About'>Press</a> </ul>
       </ol>
       </div>
       <ol>
       
-      <ul><a className='links-style' href='About'>QuickLink</a> </ul>
-      <ul><a className='links-style' href='Men'>Men</a> </ul>
-      <ul><a className='links-style' href='Women'>Women</a> </ul>
+      <ul><a className='links-style' href='/About'>QuickLink</a> </ul>
+      <ul><a className='links-style' href='/Men'>Men</a> </ul>
+      <ul><a className='links-style' href='/Women'>Women</a> </ul>
       </ol>
       <ol>
-      <ul><a className='links-style' href='About'>FAQ</a> </ul>
-      <ul><a className='links-style' href='About'>Cancellation</a> </ul>
-      <ul><a className='links-style' href='About'>Return</a> </ul>
+      <ul><a className='links-style' href='/About'>FAQ</a> </ul>
+      <ul><a className='links-style' href='/About'>Cancellation</a> </ul>
+      <ul><a className='links-style' href='/About'>Return</a> </ul>
       </ol>
       <ol>
-      <ul><a className='links-style' href='About'>TopPicks</a> </ul>
-      <ul><a className='links-style' href='About'>Fashion</a> </ul>
-      <ul><a className='links-style' href='About'>Footwear</a> </ul>
-      <ul><a className='links-style' href='About'>Skincare</a> </ul>
+      <ul><a className='links-style' href='/About'>TopPicks</a> </ul>
+      <ul><a className='links-style' href='/About'>Fashion</a> </ul>
+      <ul><a className='links-style' href='/About'>Footwear</a> </ul>
+      <ul><a className='links-style' href='/About'>Skincare</a> </ul>
        
 
       </ol>
